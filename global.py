b567f37af81350b2fb229de307f1cf39891bd886
alex <alex.bertolla@gmail.com>
2020-04-13 16:29:46 -0300

    Feature extractor

--- global.py
+++ global.py
@@ -30,11 +30,11 @@ def fd_halarick(image):
     return halarick
 
 #COLOR HISTOGRAM DESCRIPTOR
-def color_histogram(image, mask=None):
+def fd_color_histogram(image, mask=None):
     image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
     hist = cv2.calcHist([image], [0, 1, 2], None, [bins, bins, bins], [0, 256, 0, 256, 0, 256])
     cv2.normalize(hist, hist)
-    return hist.flattern()
+    return hist.flatten()
 
 
 #get the training labels
@@ -63,9 +63,63 @@ for training_name in train_labels:
 
         #read the image and resize it to a fixed-size
         image = cv2.imread(file)
-        print(image)
+        image = cv2.resize(image, fixed_size)
 
+        ####################################
+        # Global Feature Extraction
+        ####################################
 
+        fv_hu_moments = fd_hu_moments(image)
+        fv_halarick = fd_halarick(image)
+        fv_histogram = fd_color_histogram(image)
 
+        ####################################
+        # concatenating global features
+        ####################################
+
+        global_feature = np.hstack([fv_histogram, fv_halarick, fv_hu_moments])
+
+        #update the list of labels and feature vectors
+        labels.append(current_label)
+        global_features.append(global_feature)
+
+    print("[STATUS] processed folder: {}".format(current_label))
+
+print ("##############################################################")
+
+print("[STATUS] completed Global Feature Extraction...")
+
+# get the overall feature vector size
+print("[STATUS] feature vector size {}".format(np.array(global_features).shape))
+
+# get the overall training label size
+print("[STATUS] training Labels {}".format(np.array(labels).shape))
+
+#encode the target labels
+targetNames = np.unique(labels)
+le = LabelEncoder()
+target = le.fit_transform(labels)
+print("[STATUS] training labels encoded...")
+
+#scale features in the range (0-1)
+scaler = MinMaxScaler(feature_range=(0, 1))
+rescaled_features = scaler.fit_transform(global_features)
+print("[STATUS] feature vector normalized...")
+
+print("[STATUS] target labels: {}".format(target))
+print("[STATUS] target labels shape: {}".format(target.shape))
+
+#save the feature vector using HDF5
+h5f_data = h5py.File(h5_data, 'w')
+h5f_data.create_dataset('dataset_1', data=np.array(rescaled_features))
+
+h5f_label = h5py.File(h5_labels, 'w')
+h5f_label.create_dataset('dataset_1', data=np.array(target))
+
+h5f_data.close()
+h5f_label.close()
+
+print("[STATUS] end of training..")
+print ("##############################################################")
 
 print('FIM')
